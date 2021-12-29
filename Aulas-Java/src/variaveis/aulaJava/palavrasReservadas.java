ce8462e548679c8ba23105373279f6a6719f92a6
CaioLFEsteves <caiolfesteves@gmail.com>
2021-12-29 15:10:55 -0300

    Demonstração de valor de variavel sem declaração

--- Aulas-Java/src/variaveis/aulaJava/palavrasReservadas.java
+++ Aulas-Java/src/variaveis/aulaJava/palavrasReservadas.java
@@ -2,4 +2,21 @@ package variaveis.aulaJava;
 
 public class palavrasReservadas {
 
+	public static void main(String[] args) {
+		
+		
+		/*Valor inicial declarando ou não, é zero.*/
+		
+		int numero;
+		
+		int numero2 = 0;
+		
+		System.out.println(numero2);
+		
+		
+		
+	}
+	
+	
+	
 }
