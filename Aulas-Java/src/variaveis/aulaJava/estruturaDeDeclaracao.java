3e811fa00718e16fb6cb2454adea06ea29776b64
CaioLFEsteves <caiolfesteves@gmail.com>
2021-12-29 15:23:38 -0300

    Estrutura de declaração e Concatenação.

---
+++ Aulas-Java/src/variaveis/aulaJava/estruturaDeDeclaracao.java
@@ -0,0 +1,25 @@
+package variaveis.aulaJava;
+
+public class estruturaDeDeclaracao {
+
+	
+		public static void main(String[] args) {
+				
+				boolean teste;
+				boolean teste2;
+				
+				int numero;
+				int numero2;
+				
+				int maiorIdade = 18;
+				
+				int idosoIdade = 60;
+				
+				int jovemIdade = 16;
+				
+				String cpf = "198.165.154-79";
+				
+				System.out.println("Maior idade é = " + maiorIdade);
+				System.out.println("Idoso a idade é maior que = " + idosoIdade);
+			}
+}
