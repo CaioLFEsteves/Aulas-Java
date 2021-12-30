928a2939313edf274555429478354bf112403db1
CaioLFEsteves <caiolfesteves@gmail.com>
2021-12-30 15:43:37 -0300

    Aula sobre variaveis tipo Char e String

---
+++ Aulas-Java/src/variaveis/aulaJava/variaveisTipoChar.java
@@ -0,0 +1,24 @@
+package variaveis.aulaJava;
+
+public class variaveisTipoChar {
+
+	public static void main(String[] args) {
+		
+		
+		/*CHAR é sempre uma única letra, e fica entre aspas simples*/
+		char pessoaFisica = 'F';
+		char pessoaJuridica = 'J';
+		
+		char pessoaMasculino = 'M';
+		char pessoaFeminino = 'F';
+		
+		if(pessoaFeminino == 'F') {
+			System.out.println("Feminino");
+		}
+		else {
+			System.out.println("Masculino");
+		}
+		
+		
+	}
+}
---
+++ Aulas-Java/src/variaveis/aulaJava/variaveisTipoStringEConcatenacao.java
@@ -0,0 +1,22 @@
+package variaveis.aulaJava;
+
+public class variaveisTipoStringEConcatenacao {
+
+	
+	public static void main(String[] args) {
+		
+		/*String no java pode ser usado para textos de tamanho qualquer*/
+		
+		String textoX = "skdjdojqodnjrbffbifnofneksadp4737394732nqjdndnqudldn";
+		
+		String nome = "Caio L. F. Esteves";
+		String cpf = "XXX.XXX.XXX-XX";
+		String telefone = "(XX) X XXXX-XXXX";
+		String endereço = "Santo Ant° da Platina - PR";
+		int idade = 20;
+		
+		System.out.println("Meu nome é: " + nome + ". Meu CPF é: " + cpf +
+		". Meu tefone de contato é: " + telefone + ". Moro em: " + endereço +
+		" e tenho " + idade + " anos.");
+	}
+}
