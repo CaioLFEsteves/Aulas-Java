540c2f34d5a9a8c1f9d1bd3a131fd4258838174d
CaioLFEsteves <caiolfesteves@gmail.com>
2022-01-01 16:00:41 -0300

    Estrutura de Repetição com Break

---
+++ Aulas-Java/src/operacoesLCeR/aulaJava/estrututraRepeticaoCBreak.java
@@ -0,0 +1,19 @@
+package operacoesLCeR.aulaJava;
+
+public class estrututraRepeticaoCBreak {
+
+	public static void main(String[] args) {
+		
+		/*ESTRTUTRA DA REPETIÇÃO FOR COM BREAK*/
+		
+		
+		for (int num1 = 0; num1 <= 10; num1 ++) {
+			if(num1 == 7) {
+			System.out.println("Número 7 encontrado");
+			break; /*Assim que o método é realizado, o programa para a execução.*/
+			}
+		}
+		
+		
+	}
+}
